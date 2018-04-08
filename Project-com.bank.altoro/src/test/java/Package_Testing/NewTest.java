1ac74cfd180a0ab0f56c9ad4608a915f9f29664f
temp <temp@rajat-PC>
2018-04-08 10:24:39 +0530

    right

--- Project-com.bank.altoro/src/test/java/Package_Testing/NewTest.java
+++ Project-com.bank.altoro/src/test/java/Package_Testing/NewTest.java
@@ -3,7 +3,7 @@ package Package_Testing;
 public class NewTest {
 
 	public static void main(String[] args) {
-		System.out.println("New change is good but not that good1");
+		System.out.println("New change is good but not that bad1");
 
 	}
 
