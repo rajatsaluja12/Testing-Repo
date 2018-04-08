cf78b3f5a119e512d71776e851a7f01ee1f2a05c
rajatsaluja12 <37304494+rajatsaluja12@users.noreply.github.com>
2018-04-08 10:26:45 +0530

    commitnew

--- Project-com.bank.altoro/src/test/java/Package_Testing/NewTest.java
+++ Project-com.bank.altoro/src/test/java/Package_Testing/NewTest.java
@@ -3,7 +3,7 @@ package Package_Testing;
 public class NewTest {
 
 	public static void main(String[] args) {
-		System.out.println("New change is good but not that bad1");
+		System.out.println("New rajat is experienced now");
 
 	}
 
