72bc3101aa74c64d7bc2e4821556c61ac4392f40
rajatsaluja12 <37304494+rajatsaluja12@users.noreply.github.com>
2018-04-08 10:31:29 +0530

    commit true

--- Project-com.bank.altoro/src/test/java/Package_Testing/NewTest.java
+++ Project-com.bank.altoro/src/test/java/Package_Testing/NewTest.java
@@ -3,7 +3,7 @@ package Package_Testing;
 public class NewTest {
 
 	public static void main(String[] args) {
-		System.out.println("Pixy is bad girl");
+		System.out.println("rajat is good boy");
 
 	}
 
